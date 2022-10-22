6200ec7b51a92d51abe3805abdade3ed14a5261b
ABChapagain <achyutchapagain05@gmail.com>
2022-10-22 09:52:51 +0545

    Final Production Commit

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import colors from 'colors'
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
@@ -10,21 +11,33 @@ import postRoute from './routes/postRoute.js'
 import userRoute from './routes/userRoute.js'
 
 const app = express()
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 dotenv.config()
 connectDB()
 
-app.get('/', (req, res) => {
-  res.send('App is running well.')
-})
-
 app.use('/api/posts', postRoute)
 app.use('/api/users', userRoute)
 
+const __dirname = path.resolve()
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('App is running well.')
+  })
+}
+
 app.use(notFound)
 app.use(errorHandler)
+
+
 
 const PORT = process.env.PORT || 5000
 

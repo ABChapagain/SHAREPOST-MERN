3998efb12dd4efa34dd0a6d8b3a66a79b643a268
ABChapagain <achyutchapagain05@gmail.com>
2022-10-22 11:10:28 +0545

    Final Final

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ app.use('/api/users', userRoute)
 
 const __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './frontend/build')))
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './frontend/build/index.html'))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   })
 } else {
   app.get('/', (req, res) => {
@@ -36,8 +36,6 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(notFound)
 app.use(errorHandler)
-
-
 
 const PORT = process.env.PORT || 5000
 
